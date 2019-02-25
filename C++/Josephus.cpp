0cf2f6cffb536f7bc0c3dd3a5d90e3d50d758125
aaa <aaa>
2019-02-25 18:16:04 -0500

    Fixed some stuff

--- C++/Josephus.cpp
+++ C++/Josephus.cpp
@@ -3,11 +3,12 @@
 #include <string>
 using namespace std;
 
-void Killing(queue<string> que, int jumps);
+void Killing(queue<string> que, size_t jumps);
+
 queue<string> que;
 string temp = "";
 string test = "";
-int jum;
+size_t jum;
 
 int main() {
 	cout << "Ingrese los nombres de las personas " << endl;
@@ -29,7 +30,7 @@ int main() {
 	Killing(que, jum);
 	return 0;
 }
-void Killing(queue<string> que, int jumps) {
+void Killing(queue<string> que, size_t jumps) {
 	string hi;
 	for (size_t z = 0; z < jumps-1; ++z) {
 		hi = que.front();
@@ -37,13 +38,13 @@ void Killing(queue<string> que, int jumps) {
 		que.push(hi);
 	}
 	while (que.size() > 1) {
-		for (size_t i = 0; i < jumps; ++i) {
+		cout << "Se ha matado a: " << que.front() << endl;
+		que.pop();
+		for (size_t i = 0; i < jumps-1; ++i) {
 			hi = que.front();
 			que.pop();
 			que.push(hi);
 		}
-		cout << "Se ha matado a: " << que.front() << endl;
-		que.pop();
 	}
 	cout << "Sobrevivio " << que.front() << endl;
 }
